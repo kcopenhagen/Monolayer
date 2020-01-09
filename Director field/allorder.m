b003942789c1adfbd0b0b63106cd570262554aa6
Katie Copenhagen <kcopenhagen01@gmail.com>
2020-01-09 14:16:15 -0500

    Calculate and save the order field for all experiments.

--- Director field/allorder.m
+++ Director field/allorder.m
@@ -1,22 +1,22 @@
 function allorder(datapath)
-    folders = datapath;
 
-    fpath = [uigetdir(folders) '/'];
-    
-    files = dir([fpath '/Laser/']);
-    dirFlags = [files.isdir];
-    files(dirFlags) = [];
-    del = [];
-    for t = 1:numel(files)
-        if files(t).name(1) == '.'
-            del = [del; t];
+    fpaths = getfold(datapath);
+    for f = 1:numel(fpaths)
+        fpath = fpaths{f};
+        files = dir([fpath '/Laser/']);
+        dirFlags = [files.isdir];
+        files(dirFlags) = [];
+        del = [];
+        for t = 1:numel(files)
+            if files(t).name(1) == '.'
+                del = [del; t];
+            end
+        end
+        files(del) = [];
+
+        N = numel(files);
+        for t = 1:N
+            nemorderfield(fpath,t);
         end
     end
-    files(del) = [];
-
-    N = numel(files);
-    for t = 1:N
-        nemorderfield(fpath,t);
-    end
-    
 end
