f8c5d15efc984bee82c8d3aa6599dd0650288b85
Katie Copenhagen <kcopenhagen01@gmail.com>
2020-01-09 14:55:11 -0500

    Calculate and plot the angle between the direction the defect is moving and it's axis of symmetry.

--- Director field/Defect analysis/directedmotion.m
+++ Director field/Defect analysis/directedmotion.m
@@ -2,16 +2,8 @@ function directedmotion(datapath)
 
 addpath('../');
 
-folders = dir(datapath);
-dF = [folders.isdir];
-folders = folders(dF);
-del = [];
-for f = 1:numel(folders)
-    if folders(f).name(1) == '.'
-        del = [del; f];
-    end
-end
-folders(del) = [];
+folders = getfold(datapath);
+
 vps = [];
 vns = [];
 aps = [];
@@ -23,10 +15,10 @@ for tau = 1:5:51
 
     for f = 1:numel(folders)
 
-        fpath = [folders(f).folder '/' folders(f).name '/'];
+        fpath = folders{f};
         XYcal = getXYcal(fpath);
 
-        adefs= alldefects(fpath);
+        load([fpath 'adefs.mat'],'adefs');
 
         ids = unique([adefs.id]);
         for ij = 1:numel(ids)
@@ -136,7 +128,7 @@ xticklabels(["0"; "\pi/6"; "\pi/3"; "\pi/2"; "2\pi/3"; "5\pi/6"; "\pi"])
 %% v plot
 
 figure
-eds = 0:0.1:5;
+eds = 0:0.1:10;
 pvs = histcounts(vps,eds,'Normalization','pdf');
 nvs = histcounts(vns,eds,'Normalization','pdf');
 
@@ -146,29 +138,3 @@ plot(eds(2:end)-eds(1)/2,nvs,'b','LineWidth',2);
 set(gca,'FontSize',18);
 xlabel('Speed (\mum /min)');
 ylabel('PDF');
-%% Test plots
-eds = 0:pi/36:pi;
-figure
-plot(eds(2:end) - eds(1)/2,pangs1,'Color',[1 0 0],'LineWidth',2);
-hold on
-plot(eds(2:end) - eds(1)/2,pangs5,'Color',[0.8 0 0],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,pangs9,'Color',[0.6 0 0],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,pangs11,'Color',[0.4 0 0],'LineWidth',2);
-
-plot(eds(2:end) - eds(1)/2,nangs1,'Color',[0 0 1],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,nangs5,'Color',[0 0 0.8],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,nangs9,'Color',[0 0 0.6],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,nangs11,'Color',[0 0 0.4],'LineWidth',2);
-
-eds = 0:0.5:15;
-figure
-plot(eds(2:end) - eds(1)/2,pvs1,'Color',[1 0 0],'LineWidth',2);
-hold on
-plot(eds(2:end) - eds(1)/2,pvs5,'Color',[0.8 0 0],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,pvs9,'Color',[0.6 0 0],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,pvs11,'Color',[0.4 0 0],'LineWidth',2);
-
-plot(eds(2:end) - eds(1)/2,nvs1,'Color',[0 0 1],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,nvs5,'Color',[0 0 0.8],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,nvs9,'Color',[0 0 0.6],'LineWidth',2);
-plot(eds(2:end) - eds(1)/2,nvs11,'Color',[0 0 0.4],'LineWidth',2);
